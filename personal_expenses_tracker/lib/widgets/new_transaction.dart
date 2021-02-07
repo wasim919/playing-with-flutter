a053d668b3b2c272307ddc0959d463b2351c0a44
Wasim <wasimwik919@gmail.com>
2021-02-07 23:40:03 +0530

    modal bug fixed

--- personal_expenses_tracker/lib/widgets/new_transaction.dart
+++ personal_expenses_tracker/lib/widgets/new_transaction.dart
@@ -1,10 +1,15 @@
 import "package:flutter/material.dart";
 
-class NewTransaction extends StatelessWidget {
-  final titleInput = TextEditingController();
-  final amountInput = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
   NewTransaction(this.addNewTransaction);
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleInput = TextEditingController();
+  final amountInput = TextEditingController();
 
   void submitTransaction() {
     String title = titleInput.text;
@@ -12,7 +17,7 @@ class NewTransaction extends StatelessWidget {
     if (title.isEmpty || amount <= 0) {
       return;
     }
-    addNewTransaction(title, amount);
+    widget.addNewTransaction(title, amount);
   }
 
   @override
@@ -27,7 +32,7 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleInput,
-              onSubmitted: (_) => addNewTransaction(),
+              onSubmitted: (_) => submitTransaction(),
             ),
             TextField(
               keyboardType: TextInputType.number,
