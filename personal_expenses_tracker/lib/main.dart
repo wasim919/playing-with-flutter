2e18cf5157af7b36cf759ace3a5e4fc195677d8a
Wasim <wasimwik919@gmail.com>
2021-02-07 23:30:26 +0530

    modal bottom sheet added

--- personal_expenses_tracker/lib/main.dart
+++ personal_expenses_tracker/lib/main.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 
-import "./widgets/user_transactions.dart";
+import './widgets/transaction_list.dart';
+import "./models/transaction.dart";
+import "./widgets/new_transaction.dart";
 
 void main() => runApp(MyApp());
 
@@ -14,12 +16,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 59.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: "Daily Groceries",
+      amount: 20.99,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+    setState(() {
+      _transactions.add(newTx);
+    });
+  }
+
+  void startNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        builder: (BuildContext bctx) {
+          return NewTransaction(_addNewTransaction);
+        },
+        context: ctx);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Personal Expenses"),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.add,
+            ),
+            onPressed: () {
+              startNewTransaction(context);
+            },
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -29,8 +81,17 @@ class MyHomePage extends StatelessWidget {
               child: Text("Charts"),
               color: Colors.blue,
             ),
-            UserTransactions(),
+            TransactionList(_transactions),
           ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          startNewTransaction(context);
+        },
+        child: Icon(
+          Icons.add,
         ),
       ),
     );
